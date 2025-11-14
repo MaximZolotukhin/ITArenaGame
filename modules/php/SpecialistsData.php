6c4e1bddf8edeb49377e30438ef67e1959b00d59
VelesDragon <ScorpionEvil777@gmai.com>
2025-11-14 10:35:22 +0300

    Добавил одного специалиста

--- modules/php/SpecialistsData.php
+++ modules/php/SpecialistsData.php
@@ -11,7 +11,58 @@ namespace Bga\Games\itarenagame;
  */
 class SpecialistsData
 {
-    private const CARD_COUNT = 110;
+
+    /*
+    Отделы: 
+        sales-department - Отдел продаж
+        back-office - Бэк офис
+        technical-department - Техотдел
+        universal - Универсальный
+
+    Цвета
+    #FFFF00 - Желтый
+    #0000FF - Синий
+    #008000 - Зеленый 
+    #800000 -  Красный 
+    */
+    private const CARD_COUNT = [
+        1 => [
+            'id' => 1,
+            'type' => 'specialist', // Тип карты
+            'typeName' => 'specialist', // Тип карты название на русском
+            'price' => 1, // Цена карты
+            'name' => 'Дмитрий', // Имя специалиста
+            'color' => '#0000FF', // Цвет специалиста
+            'speciality' => 'Веб–программист', // Специальность специалиста
+            'effect' => '', // Эффект
+            'department' => 'technical-department', // Отдел
+            'effectDescription' => 'Улучшите на 1 розовый трек в техотделе', // Описание эффекта
+            'starterOrFinisher' => 'F', // Стартер или финишер
+            'management' => null, // Управление
+            'firstGame' => false, // Если первая игра то будет доступны только 4 карты
+            'additionalSkill' => null, // Дополнительный навык
+            'victoryPoints' => 0, // Очки победы
+            'img' => 'img/specialists/technical-department/1.png', // Изображение
+        ],
+        2 => [
+            'id' => 2,
+            'type' => 'specialist', // Тип карты
+            'typeName' => 'specialist', // Тип карты название на русском
+            'price' => 1, // Цена карты
+            'name' => 'Дмитрий', // Имя специалиста
+            'color' => '#0000FF', // Цвет специалиста
+            'speciality' => 'Веб–программист', // Специальность специалиста
+            'effect' => '', // Эффект
+            'department' => 'technical-department', // Отдел
+            'effectDescription' => 'Улучшите на 1 розовый трек в техотделе', // Описание эффекта
+            'starterOrFinisher' => 'F', // Стартер или финишер
+            'management' => null, // Управление
+            'firstGame' => false, // Если первая игра то будет доступны только 4 карты
+            'additionalSkill' => null, // Дополнительный навык
+            'victoryPoints' => 0, // Очки победы
+            'img' => 'img/specialists/technical-department/1.png', // Изображение
+        ],
+    ];
 
     /**
      * Шаблон карточки специалиста для удобства заполнения.
